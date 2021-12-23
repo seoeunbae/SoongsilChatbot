ffe9d97d62d477efc94c7eca81191f1571634ace
SeoeunBae <enyoupark@gmail.com>
2021-12-23 20:39:44 +0900

    add updateGisikFoods

--- src/main/java/ssu/haksik/haksik/common/crawling/GisikCrawling.java
+++ src/main/java/ssu/haksik/haksik/common/crawling/GisikCrawling.java
@@ -1,26 +1,24 @@
 package ssu.haksik.haksik.common.crawling;
 
+import lombok.RequiredArgsConstructor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import ssu.haksik.haksik.gisik.Gisik;
 import ssu.haksik.haksik.gisik.GisikRepository;
 
 import java.io.IOException;
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
-import java.util.List;
 
-@Service
+@Component
+@RequiredArgsConstructor
 public class GisikCrawling {
     private final GisikRepository gisikRepository;
-
-    public GisikCrawling(GisikRepository gisikRepository){
-        this.gisikRepository=gisikRepository;
-    }
 
     private Elements crawling() throws IOException {
         String URL = "https://ssudorm.ssu.ac.kr:444/SShostel/mall_main.php?viewform=B0001_foodboard_list&board_no=1";
@@ -31,8 +29,9 @@ public class GisikCrawling {
         return tr;
     }
 
+    @Transactional
     @Scheduled(cron = "0 0 1 * * 1")
-    private void saveGisik() throws IOException {
+    void saveGisik() throws IOException {
         Elements tr= this.crawling();
         for (int day=1;day<8;day++) {
             Elements td = tr.get(day).getElementsByTag("td");
@@ -40,20 +39,20 @@ public class GisikCrawling {
                 Element timeElement = td.get(time);
                 String foods = timeElement.html().replace("<br>", "\n").replace(" ", "").replace("&amp;", "&");
                 String result = "<오늘의 메뉴>\n\n".concat(foods);
-                Gisik gisikExist = this.gisikRepository.findByEatingTimeAndDay(day,time);
+                Gisik gisikExist = this.gisikRepository.findByEatingTimeAndDay(time, day);
 
                 if (gisikExist == null) {
                     Gisik gisik = new Gisik(result, day, time);
                     this.gisikRepository.save(gisik);
-                } else{
-                    Gisik gisik = gisikRepository.findByEatingTimeAndDay(day, time);
-                    gisik.setFoods(result);
+                } else {
+                    this.gisikRepository.updateGisikFoods(result, day, time);
                 }
             }
         }
 
     }
 
+    @Transactional(readOnly = true)
     public String getGisik(int eatingTime){
         DayOfWeek dayOfWeek = LocalDateTime.now().getDayOfWeek();
         int day = dayOfWeek.getValue();
