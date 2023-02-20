d361f950bf949f402fe3519879000eb2108c8704
seoeunbae <enyoupark@gmail.com>
2023-02-20 16:52:32 +0900

    refactor : dodamController - restapi 규격으로 수정

--- src/main/java/ssu/haksik/haksik/dodam/controller/DodamController.java
+++ src/main/java/ssu/haksik/haksik/dodam/controller/DodamController.java
@@ -2,10 +2,7 @@ package ssu.haksik.haksik.dodam.controller;
 
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ssu.haksik.haksik.common.enums.EatingTime;
 import ssu.haksik.haksik.common.response.*;
 import ssu.haksik.haksik.dodam.service.DodamService;
@@ -17,12 +14,12 @@ public class DodamController {
 
     private final DodamService dodamService;
 
-    @PostMapping() // 카카오 API에서 post 방식만 지원함
-    public FoodResponse getDodamFood(@RequestParam EatingTime eatingTime){
+    @PostMapping("/eatingTime/{eatingTime}") // 카카오 API에서 post 방식만 지원함
+    public FoodResponse getDodamFood(@PathVariable(value = "eatingTime") EatingTime eatingTime){
         return dodamService.getDodamFood(eatingTime);
     }
 
-    @PostMapping("/save") // 카카오 API에서 post 방식만 지원함
+    @PostMapping() // 카카오 API에서 post 방식만 지원함
     public void saveDodamFood() throws IOException {
         dodamService.saveDodamFoodMenu();
     }
